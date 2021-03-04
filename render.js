580366791708577766b3962a9735132ae24d599f
ooa113y <13thSlayer@gmail.com>
2021-03-04 15:26:27 +0300

    Add backlog functionality

--- render.js
+++ render.js
@@ -1,6 +1,7 @@
 /* eslint-env browser */
 const speed = 30
 function typeText (text, blockHandler) {
+  document.getElementById('textboxContainer').style.color = 'white'
   const textbox = document.getElementById('textbox')
   textbox.innerText = ''
   let i = 0
@@ -23,10 +24,23 @@ async function main () {
       await fetch(new URLSearchParams(location.search).get('file'))).text()
   ).split('\n').filter(x => x.trim())
   let currentLine = 0
+  let maxLine = 0
   function handleClick () {
+    currentLine = maxLine
     typeText(script[currentLine], handleClick)
     currentLine++
+    maxLine++
   }
+  document.body.addEventListener('wheel', event => {
+    document.getElementById('textboxContainer').style.color = 'yellow'
+    if (event.deltaY < 0) {
+      currentLine--
+    } else if (currentLine + 1 < maxLine) {
+      currentLine++
+    }
+    if (currentLine < 0) currentLine = 0
+    document.getElementById('textbox').textContent = script[currentLine]
+  })
   typeText('', handleClick)
 }
 if (window.obsstudio || new URLSearchParams(location.search).get('force') !== null) {
